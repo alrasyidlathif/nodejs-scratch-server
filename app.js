f83e87faed1dd7ef6f653c9fbf33b0c1f53b2c79
alrasyidlathif <alrasyidlathif@gmail.com>
2021-05-26 16:48:00 +0700

    send html response

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-    console.log(req)
+    console.log(req.url, req.method, req.headers)
+    res.setHeader('Content-Type', 'text/html')
+    res.write('<html>')
+    res.write('<header><title>lathif code</title></header>')
+    res.write('<body><h1>hello world</h1></body>')
+    res.write('</html>')
+    res.end()
+    // process.exit()
 })
 
 server.listen(1100)
